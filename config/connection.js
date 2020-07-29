8b750f55ea727809a7539ffe29f2ac99b4cad698
Jacob Corum <cor15031@byui.edu>
2020-07-28 20:59:40 -0600

    config modified

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,19 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // create connection to our database, pass in your MySQL information for username and password
+
 let sequelize;
+
 if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PW,
     {
-      host: "localhost",
+      host: process.env.DB_HOST,
       dialect: "mysql",
-      port: 3306,
     }
   );
 }
