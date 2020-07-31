e9c6d93daa0dc0b4ac952f98ed65214390ba9d4e
Jacob Corum <cor15031@byui.edu>
2020-07-31 15:21:18 -0600

    added logic to make nouns plural

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
+const session = require("express-session");
 const path = require("path");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
-const session = require("express-session");
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
+
+const hbs = exphbs.create({ helpers });
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
